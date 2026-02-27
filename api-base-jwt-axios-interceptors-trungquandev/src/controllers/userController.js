b738ba4407c62123dc826f1b21a5f5961f06f7eb
Nguyen Van Giang <giang16012405@gmail.com>
2026-02-27 10:37:09 +0700

    3. API Login

--- api-base-jwt-axios-interceptors-trungquandev/src/controllers/userController.js
+++ api-base-jwt-axios-interceptors-trungquandev/src/controllers/userController.js
@@ -1,5 +1,6 @@
 import { StatusCodes } from 'http-status-codes'
 import ms from 'ms'
+import { JwtProvider } from '~/providers/JwtProvider'
 
 /**
  * Mock nhanh thông tin user thay vì phải tạo Database rồi query.
@@ -31,8 +32,49 @@ const login = async (req, res) => {
     }
 
     // Trường hợp nhập đúng thông tin tài khoản, tạo token và trả về cho phía Client
+    // Tạo thông tin payload để đính kèm trong JWT Token: bao gồm id và email của user
+    const userInfo = {
+      id: MOCK_DATABASE.USER.ID,
+      email: MOCK_DATABASE.USER.EMAIL
+    }
 
-    res.status(StatusCodes.OK).json({ message: 'Login API success!' })
+    // Tạo ra 2 loại token, accessToken và refreshToken để trả về phía FE
+    const accessToken = await JwtProvider.generateToken(
+      userInfo,
+      ACCESS_TOKEN_SECRET_SIGNATURE,
+      '1h'
+    )
+
+    const refreshToken = await JwtProvider.generateToken(
+      userInfo,
+      REFRESH_TOKEN_SECRET_SIGNATURE,
+      '14 days'
+    )
+
+    /**
+     * Xử lý trường hợp trả về http only cookie cho phía trình duyệt
+     * Về cái maxAge và thư viện ms: https://expressjs.com/en/api.html
+     * Đối với cái maxAge - thời gian sống của Cookie thì chúng ta sẽ để tối đa sống 14 ngày, tùy dự án. Lưu ý thời gian sống của cookie khác với cái thời gian sống của token nhé. Đừng bị nhầm lẫn
+     */
+    res.cookie('accessToken', accessToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+      maxAge: ms('14 days')
+    })
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+      maxAge: ms('14 days')
+    })
+
+    // Trả về thông tin user cũng như sẽ trả về Tokens cho trường hợp phía FE cần lưu Tokens vào Localstorage
+    res.status(StatusCodes.OK).json({
+      ...userInfo,
+      accessToken,
+      refreshToken
+    })
   } catch (error) {
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error)
   }
--- api-base-jwt-axios-interceptors-trungquandev/src/providers/JwtProvider.js
+++ api-base-jwt-axios-interceptors-trungquandev/src/providers/JwtProvider.js
@@ -1,1 +1,34 @@
-// JwtProvider.js
+import JWT from 'jsonwebtoken'
+
+/**
+ * Funtion tạo mới một token - Cần 3 tham số đầu vào
+ * userInfo: Những thông tin muốn đính kèm vào token
+ * secretSignature: Chứ ký bí mật (dạng một chuỗi string ngẫu nhiên) trên docs thì để tên là privateKey tùy đều được
+ * tokenLife: Thời gian tồn tại của token
+ */
+const generateToken = async (userInfo, secretSignature, tokenLife) => {
+  try {
+    // Hàm sign của thư viện Jwt - Thuật toán mặc định là HS256, cứ cho vào code để nhớ kiến thức
+    return JWT.sign(userInfo, secretSignature, { algorithm: 'HS256', expiresIn: tokenLife })
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+/**
+ * Funtion kiểm tra một token có hợp lệ hay không
+ * Hợp lệ ở đây hiểu đơn giản là cái token được tạo ra có đúng với cái chữ lý bí mật secretSignature trong dự án hay không
+ */
+const verifyToken = async () => {
+  try {
+    // Hàm verify của thư viện Jwt
+    return JWT.verify(token, secretSignature)
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+export const JwtProvider = {
+  generateToken,
+  verifyToken,
+}
